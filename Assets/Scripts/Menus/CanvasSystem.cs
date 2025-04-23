4d2490e7c7c341e1dcb431a0bfffbd5371315497
Carol <ikkilink@hotmail.com>
2025-04-22 21:14:54 -0600

    felicidad

---
+++ Assets/Scripts/Menus/CanvasSystem.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class CanvasSystem : MonoBehaviour
+{
+    public Canvas[] canvasInScene;
+    public Stack<Canvas> StackPanels;
+    void Start()
+    {
+        StackPanels = new Stack<Canvas>();
+        StackPanels.Push(canvasInScene[0]);
+    }
+    void Update()
+    {
+        if (StackPanels.Count > 1 && Input.GetKeyDown(KeyCode.Escape))
+        {
+
+            StackPanels.Pop().enabled = false;
+            StackPanels.Peek().enabled = true;
+        }
+    }
+    public void GotoPanel(int canvasToEnable)
+    {
+        for (int indexPanels = 0; indexPanels < canvasInScene.Length; indexPanels++)
+        {
+            StackPanels.Push(canvasInScene[indexPanels]);
+            canvasInScene[indexPanels].enabled = indexPanels == canvasToEnable;
+        }
+    }
+}
