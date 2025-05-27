70cf5bf61445c49596027085264a7a3e535fad12
Carol <ikkilink@hotmail.com>
2025-05-27 15:57:11 -0600

    FinalScoreScene

---
+++ Assets/Scripts/GetTipFinal.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using TMPro;
+public class GetTipFinal : MonoBehaviour
+{
+    public TextMeshProUGUI FinalTip;
+    void Start()
+    {
+        //FinalTip = PatienceSystem.
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
