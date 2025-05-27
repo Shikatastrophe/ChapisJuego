ec319d9f571d565d28400d219dd985503697c5e8
amauryxd <cdmx2472@amerike.edu.mx>
2025-05-27 00:31:36 -0600

    SpawnsYQueue

---
+++ Assets/Scripts/SpawnSistem/SpawnSistem.cs
@@ -0,0 +1,65 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnSistem : QueueSystem
+{
+    public List<GameObject> clientsPrefabs;
+   GameObject clienteActual;
+    public bool clientExist;
+    public bool orderFinished;
+    public bool canMove;
+    int waitTime=240;
+    private Vector3 velocity;
+    public void Start()
+    {
+
+    }
+
+    public void Update()
+    {
+        if (canMove)
+        {
+            MoveCliente(clienteActual);
+        }
+        if (!clientExist)
+        {
+            SpawnCliente();
+        }
+        if (orderFinished)
+        {
+            clienteAtendido();
+        }
+    }
+    [ContextMenu("a")]
+    public void SpawnCliente()
+    {
+        clientExist = true;
+        int randomNumber = Random.Range(0, clientsPrefabs.Count);
+        clienteActual = Instantiate(clientsPrefabs[randomNumber], transform);
+        canMove = true;
+    }
+
+    public void MoveCliente(GameObject wich)
+    {
+        clienteActual.transform.position = Vector3.SmoothDamp(clienteActual.transform.position, Vector3.zero, ref velocity, 0.5f);
+    }
+    [ContextMenu("clienteAtendido")]
+    public void clienteAtendido()
+    {
+        canMove = false;
+        clienteActual.transform.position = Vector3.SmoothDamp(clienteActual.transform.position, new Vector3(20, 0, 0), ref velocity, 0.5f);
+        if (waitTime <= 0)
+        {
+            cleinteExplotar();
+            waitTime = 240;
+        }
+        waitTime--;
+    }
+    public void cleinteExplotar()
+    {
+        clientExist=false;
+        orderFinished=false;
+        Destroy(clienteActual);
+    }
+
+}
