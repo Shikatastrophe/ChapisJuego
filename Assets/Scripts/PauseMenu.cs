a2de222057341124717666b207732739c45c411f
Carol <ikkilink@hotmail.com>
2025-05-27 21:22:55 -0600

    que ya se vayan alb

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -3,9 +3,22 @@ using UnityEngine;
 public class PauseMenu : MonoBehaviour
 {
     public Canvas canvasToEnable;
-    void Update()
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Resume();
+            
+        }
+    }
+    public void PauseGame()
     {
         canvasToEnable.enabled = true;
         Time.timeScale = 0;
+    }
+    public void Resume()
+    {
+        canvasToEnable.enabled = false;
+        Time.timeScale = 1;
     }
 }
